67fda7d583743ab76631afddffd2776e2f07d86f
robhowley <howley.robert@gmail.com>
2019-03-19 22:18:10 -0400

    opener context

--- s3streaming/opener.py
+++ s3streaming/opener.py
@@ -8,15 +8,13 @@ from s3streaming.stream import StreamingBodyFileobj, deserialize, compression
 __all__ = ['s3_open']
 
 
-def from_s3_uri(s3_uri):
+def bucket_key_from_s3_uri(s3_uri):
     parsed = urlparse(s3_uri)
     key = parsed.path
     return dict(Bucket=parsed.netloc, Key=key[1:] if key else '')
 
 
 @contextmanager
-def s3_open(s3_uri, deserializer=deserialize.none, compression=compression.none):
-    deserializer, compression = deserializer or (lambda s: s), compression or (lambda s: s)
-
-    body = StreamingBodyFileobj(boto3.client('s3').get_object(**from_s3_uri(s3_uri))['Body'])
+def s3_open(s3_uri, boto_session, deserializer=deserialize.none, compression=compression.none):
+    body = StreamingBodyFileobj(boto_session.client('s3').get_object(**bucket_key_from_s3_uri(s3_uri))['Body'])
     yield deserializer(compression(body))
