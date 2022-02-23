90364a6fd2352d6662c745b7f66b060de38205e4
Satya Kondle <satya.kondle@fireeye.com>
2022-02-23 14:48:53 -0500

    Add ability to send parameters to get_object

--- s3streaming/opener.py
+++ s3streaming/opener.py
@@ -1,4 +1,3 @@
-
 from urllib.parse import urlparse
 from contextlib import contextmanager
 from s3streaming.stream import deserialize, compression
@@ -14,6 +13,7 @@ def bucket_key_from_s3_uri(s3_uri):
 
 
 @contextmanager
-def s3_open(s3_uri, boto_session, deserializer=deserialize.none, compression=compression.none):
-    body = boto_session.client('s3').get_object(**bucket_key_from_s3_uri(s3_uri))['Body']
+def s3_open(s3_uri, boto_session, deserializer=deserialize.none, compression=compression.none, params: dict=None):
+    # combine params with dictionary from parsing uri and send to get_object
+    body = boto_session.client('s3').get_object(**{**bucket_key_from_s3_uri(s3_uri),**(params if params is not None else {})})['Body']
     yield deserializer(compression(body._raw_stream))
